2bf332e716500ab6a0544070f5b6f0dce9272d3e
Carlos Timaury <timauryc@gmail.com>
2018-10-05 16:38:44 -0300

    refactor on client

--- public/client.js
+++ public/client.js
@@ -1,30 +1,66 @@
 $(document).ready(function () {
     console.log("hey, im ready :)")
 
-    $("#consult-btn").click(function () {
-        console.log('test get')
+    $("#cpf-input").mask('000.000.000-00', { reverse: true });
+
+    $(".button").click(function () {
+        console.log(this.id); // or alert($(this).attr('id'));
+        let inputData = getValidInput()
+        if (inputData) {
+            switch (this.id) {
+                case "consult-btn":
+                    consultCPF(inputData)
+                    break;
+                case "insert-btn":
+                    insertCPF(inputData)
+                    break;
+                case "delete-btn":
+                    deleteCPF(inputData)
+                    break;
+                case "modify-btn":
+                    modifyCPF(inputData)
+                    break;
+                default:
+                    console.log("I should not be here, anyways...42 is the answer")
+            }
+        } else {
+            displayResult("Cpf invalido")
+        }
+    });
+
+    function displayResult(result) {
+        $("#operations-display").text(JSON.stringify(result))
+    }
+
+    function getValidInput() {
         let cpfNumber = $("#cpf-input").val()
-        $.get(`/cpf/${cpfNumber}/estado`, function (data, status) {
+        if (cpfNumber.match(/[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}/)) {
+            return {
+                cpfNumber: cpfNumber,
+                cpfStatus: $('#cpf-status :selected').text()
+            }
+        } else {
+            return null
+        }
+    }
+
+    function consultCPF(inputData) {
+        $.get(`/cpf/${inputData.cpfNumber}/estado`, function (data, status) {
             console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
             displayResult("Data: " + JSON.stringify(data) + "\nStatus: " + status);
         });
-    });
+    }
 
-    $("#insert-btn").click(function () {
-        console.log('testPost')
-        let cpfNumber = $("#cpf-input").val()
-        let cpfStatus = $('#cpf-status :selected').text()
-        $.post('/cpf', { _id: cpfNumber, status: cpfStatus}, function (data, status) {
+    function insertCPF(inputData) {
+        $.post('/cpf', { _id: inputData.cpfNumber, status: inputData.cpfStatus }, function (data, status) {
             console.log("Data: " + JSON.stringify(data) + "\nStatus: " + status);
             displayResult("Data: " + JSON.stringify(data) + "\nStatus: " + status);
         });
-    });
+    }
 
-    $("#delete-btn").click(function () {
-        console.log('testDelete')
-        let cpfNumber = $("#cpf-input").val()
+    function deleteCPF(inputData) {
         $.ajax({
-            url: `/cpf/${cpfNumber}`,
+            url: `/cpf/${inputData.cpfNumber}`,
             type: 'DELETE',
             success: function (result) {
                 console.log("Data: " + JSON.stringify(result));
@@ -35,16 +71,13 @@ $(document).ready(function () {
                 displayResult('Error:', data);
             }
         });
-    });
+    }
 
-    $("#modify-btn").click(function () {
-        console.log('testPatch')
-        let cpfNumber = $("#cpf-input").val()
-        let cpfStatus = $('#cpf-status :selected').text()
+    function modifyCPF(inputData) {
         $.ajax({
             url: `/cpf/estado`,
             type: 'PATCH',
-            data: JSON.stringify({ "_id": cpfNumber, "status": cpfStatus }),
+            data: JSON.stringify({ "_id": inputData.cpfNumber, "status": inputData.cpfStatus }),
             contentType: 'application/json',
             processData: false,
             dataType: 'json',
@@ -57,10 +90,6 @@ $(document).ready(function () {
                 displayResult('Error:', data);
             }
         });
-    });
-
-    function displayResult(result){
-        $("#operations-display").text(JSON.stringify(result))
     }
 });
 
