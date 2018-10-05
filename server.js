093638e5acdb1ff5f0a84397c8d7df1cbe1650fa
Carlos Timaury <timauryc@gmail.com>
2018-10-05 11:55:25 -0300

    app dockerizada

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const PORT = 8080;
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static('public'));
@@ -14,6 +16,6 @@ require('./routes/cpf')(app);
 require('./routes/status')(app);
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('listening on port: ', process.env.PORT || 3000);
+app.listen(process.env.PORT || PORT, function () {
+    console.log('listening on port: ', process.env.PORT || PORT);
 });
